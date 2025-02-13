5aa40e4d7bf84eebf86d76ae6269b73953150ff4
victorllada <victor.sanchezllada@iesmiguelherrero.com>
2025-02-13 16:07:02 +0100

    Vista de equipos y jugadores Fixes #12

--- src/components/Tarjeta.jsx
+++ src/components/Tarjeta.jsx
@@ -1,22 +1,22 @@
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import Placeholder from 'react-bootstrap/Placeholder';
-import { useNavigate } from 'react-router-dom';
+import { Navigate, useNavigate } from 'react-router-dom';
+import DetallesEquipoPage from '../pages/DetallesEquipoPage';
 
-function Tarjeta(props) {
-    const { titulo, texto, imagen, idEquipo } = props;
+function Tarjeta({ tituloTarjeta, textoTarjeta, imagenTarjeta, textoBoton, nombreEntidad, datosObjeto }) {
     const navegar = useNavigate();
 
     return (
         <div className="d-flex justify-content-around">
             <Card style={{ width: '18rem' }}>
-                <Card.Img variant="top" src={imagen || "https://placehold.co/600x400/png"} />
+                <Card.Img variant="top" src={imagenTarjeta || "https://placehold.co/600x400/png"} />
                 <Card.Body>
-                    <Card.Title>{titulo}</Card.Title>
+                    <Card.Title>{tituloTarjeta}</Card.Title>
                     <Card.Text>
-                        {texto}
+                        {textoTarjeta}
                     </Card.Text>
-                    <Button onClick={() => { navegar(`/equipos/${idEquipo}`) }} variant="primary">Ver equipo</Button>
+                    <Button onClick={() => { navegar(`/${nombreEntidad}/${datosObjeto.id}`, { state: { entidad: datosObjeto } }) }} variant="primary">{textoBoton}</Button>
                 </Card.Body>
             </Card>
         </div>
--- src/pages/EquiposPage.jsx
+++ src/pages/EquiposPage.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import Tarjeta from "../components/Tarjeta";
 import $negocio from "../core/negocio";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container } from "react-bootstrap";
 
 // Cambiará según usuario para CRUD
 function EquiposPage() {
@@ -15,31 +16,19 @@ function EquiposPage() {
         fetchEquipos();
     }, []);
 
-    function cargarEquipos() {
-        if (equipos.length > 0) {
-            const rows = [];
-            for (let i = 0; i < equipos.length; i += 4) {
-                const rowEquipos = equipos.slice(i, i + 4);
-                rows.push(
-                    <div className="row justify-content-center" key={i}>
-                        {rowEquipos.map(equipo => (
-                            <div className="col-lg-3 col-md-4 mb-4" key={equipo.id}>
-                                <Tarjeta titulo={equipo.nombre} texto={equipo.centro} idEquipo={equipo.id} />
-                            </div>
-                        ))}
-                    </div>
-                );
-            }
-            return rows;
-        }
-    }
-
     return (
         <>
             <h1>Listado de equipos</h1><br />
-            <div className="container">
-                {cargarEquipos()}
-            </div>
+            <Container>
+                <div className="row justify-content-center">
+                    {equipos.map(equipo => (
+                        <div className="col-lg-3 col-md-4 mb-4" key={equipo.id}>
+                            <Tarjeta tituloTarjeta={equipo.nombre} textoTarjeta={equipo.centro} textoBoton="Ver equipo" imagenTarjeta={equipo.imagen} nombreEntidad="equipos" datosObjeto={equipo} />
+                        </div>
+                    ))}
+                </div>
+            </Container>
+            
 
             {/* Si tiene permisos de administracion, lista con equipos pendientes de inscripcion */}
         </>
