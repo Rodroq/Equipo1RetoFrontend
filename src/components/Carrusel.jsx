82b3c7371d46a9a163538944973c1caaa97ebc53
DESKTOP-475V9CA\Usuario <rodrigoruizdecos@gmail.com>
2025-02-14 16:23:00 +0100

    He actualizado el carrusel para que acepte un array de imagenes y he añadido datos de prueba en InicioPage

--- src/components/Carrusel.jsx
+++ src/components/Carrusel.jsx
@@ -1,22 +1,26 @@
 import { Carousel, Image } from "react-bootstrap";
 
-function Carrusel({tiempo = 3000, alto = '30em', ancho = '100%'}) {
+function Carrusel({ imagenes, tiempo = 3000, alto = '30em', ancho = '100%' }) {
     const imgStyle = {
         width: ancho,
-        height: alto
+        height: alto,
+        objectFit: 'cover'
     };
 
     return (
         <Carousel>
-            <Carousel.Item interval={tiempo}>
-                <img className="w-100" src="https://placehold.co/1920x500/png" style={imgStyle} />
-            </Carousel.Item>
-            <Carousel.Item interval={tiempo}>
-                <img className="w-100" src="https://placehold.co/1920x500/png" style={imgStyle} />
-            </Carousel.Item>
-            <Carousel.Item interval={tiempo}>
-                <img className="w-100" src="https://placehold.co/1920x500/png" style={imgStyle} />
-            </Carousel.Item>
+            {imagenes.map((imagen, index) => {
+                if (imagen[0]) {
+                    return (
+                        <Carousel.Item key={index} interval={tiempo}>
+                            <img className="w-100" src={imagen[0]} style={imgStyle} />
+                            <Carousel.Caption>
+                                <h1>{imagen[1]}</h1>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                    );
+                }
+            })}
         </Carousel>
     );
 }
