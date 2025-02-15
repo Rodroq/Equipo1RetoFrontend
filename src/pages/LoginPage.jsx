6c4ebc9e2ea8f206f941888ca5f96a0f9b80321b
victorllada <victor.sanchezllada@iesmiguelherrero.com>
2025-02-15 19:25:23 +0100

    Login page Fixes #53

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,19 +1,26 @@
-import { Button, Container } from 'react-bootstrap';
+import { Button, Card, Container } from 'react-bootstrap';
 import FloatingLabel from 'react-bootstrap/FloatingLabel';
 import Form from 'react-bootstrap/Form';
 
 function LoginPage() {
     return (
-        <Container fluid="sm">
-            <FloatingLabel controlId="floatingInput" label="Usuario" className="mb-3">
-                <Form.Control type="text" placeholder='Usuario' />
-            </FloatingLabel>
-            <FloatingLabel controlId="floatingPassword" label="Contraseña">
-                <Form.Control type="password" placeholder="Contraseña" />
-            </FloatingLabel>
-            <Button variant="primary" type="submit">
-                Iniciar sesión
-            </Button>
+        <Container fluid="sm" className="mt-5">
+            <Card className="shadow-lg p-4 border-0 rounded-4 bg-light">
+                <Card.Body>
+                    <Card.Title>Iniciar sesión</Card.Title>
+                    <Card.Text>
+                        <FloatingLabel controlId="floatingInput" label="Usuario" className="mb-3">
+                            <Form.Control type="text" placeholder='Usuario' />
+                        </FloatingLabel>
+                        <FloatingLabel controlId="floatingPassword" label="Contraseña">
+                            <Form.Control type="password" placeholder="Contraseña" />
+                        </FloatingLabel>
+                    </Card.Text>
+                    <Button variant="primary" type="submit">
+                        Iniciar sesión
+                    </Button>
+                </Card.Body>
+            </Card>
         </Container>
     );
 }
