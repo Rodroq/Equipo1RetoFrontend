e245a1fe4c27f97aae822c23e24d8038f0d5e523
victorllada <victor.sanchezllada@iesmiguelherrero.com>
2025-02-17 18:13:47 +0100

    Componente de migas de pan Fixes #22

--- src/components/MigasDePan.jsx
+++ src/components/MigasDePan.jsx
@@ -1,18 +1,53 @@
-import { Breadcrumb, BreadcrumbItem } from "react-bootstrap";
+import { useEffect, useId, useState } from "react";
+import { Breadcrumb } from "react-bootstrap";
+import { useLocation } from "react-router-dom";
+import { capitalizarPrimeraLetra } from "../utils/CadenasTexto";
 
+/**
+ * Componente migas de pan.
+ * @returns {JSX.Element} El componente migas de pan.
+ */
 function MigasDePan() {
-    const getPartesURL = () => {
-        const url = window.location.pathname;
-        return url.split('/').filter(parte => parte);
-    };
+    // ID de componente
+    const idComponente = useId();
 
-    const partesURL = getPartesURL();
+    // Estados
+    const  [partesURL, setPartesURL] = useState([]);
+    const location = useLocation();
 
+    // Efecto al cambiar el 'location'
+    useEffect(() => {
+        const url = location.pathname;
+        setPartesURL(url.split('/').filter(parte => parte));
+    }, [location]);
+
+    // Si partesURL está vacío no se renderiza nada
+    if (partesURL.length === 0) {
+        return null;
+    }
+
+    // Variable para almacenar el pathURL acumulado para cada elemento
+    let pathAcumulado = "";
+
+    // Renderizado
     return (
-        <Breadcrumb>
-            {partesURL.length && partesURL.map((parteUrl, index) => <Breadcrumb.Item key={index} active={partesURL.length}>{parteUrl}</Breadcrumb.Item>)}
+        <Breadcrumb className="bg-light p-2 m-2 border border-primary rounded">
+            <Breadcrumb.Item href="/">Inicio</Breadcrumb.Item>
+            {partesURL.map((parteUrl, index) => {
+                pathAcumulado += `/${parteUrl}`;
+
+                return (
+                    <Breadcrumb.Item 
+                        key={`${idComponente}-${index}`} 
+                        href={pathAcumulado} 
+                        active={index === partesURL.length - 1}
+                    >
+                        {capitalizarPrimeraLetra(parteUrl)}
+                    </Breadcrumb.Item>
+                );
+            })}
         </Breadcrumb>
-      );
+    );
 }
 
 export default MigasDePan;
