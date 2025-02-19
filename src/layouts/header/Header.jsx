690c12aacddd20caf5350ece86daafcf2604aeaa
victorllada <victor.sanchezllada@iesmiguelherrero.com>
2025-02-19 16:02:24 +0100

    Diseño Header

--- src/layouts/header/Header.jsx
+++ src/layouts/header/Header.jsx
@@ -10,52 +10,54 @@ function Header() {
   const navegar = useNavigate();
 
   return (
-    <Navbar expand="lg" bg="white" shadow="sm" className="border-bottom">
-      <Container>
-        {/* LOGO */}
-        <Navbar.Brand href="/" className="fw-bold">
-          <img src={logo} alt="Logo" style={{ height: '40px', marginRight: '10px' }} />
-          Liga Solidaria FP
-        </Navbar.Brand>
+    <header className="sticky-top border-bottom border-primary">
+      <Navbar expand="lg" bg="light" shadow="sm" className="border-bottom">
+        <Container>
+          {/* LOGO */}
+          <Navbar.Brand href="/" className="fw-bold link-hover m-0" title="Ir a Inicio.">
+            <img src={logo} alt="Logo" style={{ height: '110px' }} className="logo" />
+            <i className="bi bi-0-square"></i>
+          </Navbar.Brand>
 
-        {/* BOTÓN MENU RESPONSIVO */}
-        <Navbar.Toggle aria-controls="navbar-nav" />
+          {/* BOTÓN MENU RESPONSIVO */}
+          <Navbar.Toggle aria-controls="navbar-nav" />
 
-        {/* NAVEGACIÓN */}
-        <Navbar.Collapse id="navbar-nav">
-          <Nav className="mx-auto">
-            <Nav.Item>
-              <Nav.Link href="/torneo" className="fw-semibold text-dark link-hover">
-                Torneo
-              </Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link href="/equipos" className="fw-semibold text-dark link-hover">
-                Equipos
-              </Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link href="/retos" className="fw-semibold text-dark link-hover">
-                Retos
-              </Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link href="/publicaciones" className="fw-semibold text-dark link-hover">
-                Publicaciones
-              </Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link href="/inscripcion" className="fw-semibold text-dark link-hover">
-                Inscripción
-              </Nav.Link>
-            </Nav.Item>
-          </Nav>
+          {/* NAVEGACIÓN */}
+          <Navbar.Collapse id="navbar-nav">
+            <Nav className="mx-auto">
+              <Nav.Item>
+                <Nav.Link href="/torneo" title="Ir a Torneo." className="fw-semibold text-dark link-hover">
+                  Torneo
+                </Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link href="/equipos" title="Ir a Equipos." className="fw-semibold text-dark link-hover">
+                  Equipos
+                </Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link href="/retos" title="Ir a Retos." className="fw-semibold text-dark link-hover">
+                  Retos
+                </Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link href="/publicaciones" title="Ir a Publicaciones." className="fw-semibold text-dark link-hover">
+                  Publicaciones
+                </Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link href="/inscripcion" title="Ir a Inscripción." className="fw-semibold text-dark link-hover">
+                  Inscripción
+                </Nav.Link>
+              </Nav.Item>
+            </Nav>
 
-          {/* BOTÓN LOGIN */}
-          <Button onClick={() => { navegar('/login') }} variant="outline-primary">Login</Button>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+            {/* BOTÓN LOGIN */}
+            <Button onClick={() => { navegar('/login') }} variant="outline-primary">Login</Button>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
   );
 };
 
