1462764d8e6446e27814de3de3274b7f048f57dc
victorllada <victor.sanchezllada@iesmiguelherrero.com>
2025-03-04 17:44:24 +0100

    Añadir logos institucionales y reubicar elementos

--- src/layouts/footer/Footer.jsx
+++ src/layouts/footer/Footer.jsx
@@ -1,4 +1,5 @@
 import { Container, Row, Col, Nav } from 'react-bootstrap';
+import logos_institucionales from '../../assets/logos_institucionales.png';
 
 function Footer() {
   return (
@@ -13,25 +14,31 @@ function Footer() {
             </span>
           </Col>
 
-          {/* Columna para enlaces... */}
-          <Col className="d-flex align-items-center">
-            <span className="mb-3 mb-md-0 text-body-secondary fw-bold">
-              {/* <i className="bi bi-c-circle me-1"></i> */}
-              <a href="/privacidad">Política de privacidad</a>
-            </span>
-          </Col>
-
           {/* Columna con los enlaces de redes sociales */}
           <Col className="d-flex justify-content-end">
             <Nav className="list-unstyled d-flex">
-              <Nav.Item>
-              </Nav.Item>
               <Nav.Item>
                 <Nav.Link href="https://www.instagram.com/" title='Ir a Instagram.' target="_blank" rel="noopener noreferrer" className="text-body-secondary ms-3">
                   <i className="bi bi-instagram link-hover" style={{ fontSize: '24px'}}></i>
                 </Nav.Link>
               </Nav.Item>
             </Nav>
+          </Col>
+        </Row>
+
+        <Row className="w-100 mt-3">
+          {/* Columna con las imágenes de colaboradores */}
+          <Col className="d-flex justify-content-center">
+            <img src={logos_institucionales} alt="Logos Institucionales" className="w-75" />
+          </Col>
+        </Row>
+
+        <Row className="w-100 mt-3">
+          {/* Columna para el enlace de privacidad */}
+          <Col className="d-flex justify-content-center">
+            <span className="mb-3 mb-md-0 fw-bold">
+              <a href="/privacidad" className='text-body-secondary link-hover' title='Ir a Política de privacidad.'><small>Política de privacidad</small></a>
+            </span>
           </Col>
         </Row>
       </Container>
