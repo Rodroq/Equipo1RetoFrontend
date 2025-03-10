cf60011e836c6a3fa21aebee5f0a6e2f1ad5019a
Isalvan <isalvan12345@gmail.com>
2025-03-10 14:48:04 +0100

    Arreglado el acceso a gestion por roles

--- src/components/RutaPrivada.jsx
+++ src/components/RutaPrivada.jsx
@@ -11,12 +11,7 @@ import { AppContext } from '../contexts/AppProvider';
 function RutaPrivada({ children, rolesPermitidos }) {
     // Rol del usuario
     const {rol} = useContext(AppContext);
-
-    // Si el usuario no tiene rol, le mandamos a la página de login
-    if (!rol || rol == '') {
-        return <Navigate to="/login" replace />;
-    }
-
+    
     // Si el usuario no tiene los permisos necesarios para entrar, le mandamos a HomePage
     if (!rolesPermitidos.includes(rol)) {
         return <Navigate to="/" replace />;
