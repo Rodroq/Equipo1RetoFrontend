87ebed1c39942be00c18d68a66ed61ba3f7a6e0f
cizacas <ciza01@educantabria.es>
2025-03-14 09:57:10 +0100

    cambiados los patrocinadores

--- src/data/Patrocinadores.js
+++ src/data/Patrocinadores.js
@@ -1,15 +1,25 @@
-import disgarsa from "../assets/patrocinadores/disgarsa.png";
-import emi from "../assets/patrocinadores/EMI_hc.png";
-import escuela from "../assets/patrocinadores/ESCUELA_ROJO.png";
-import pitma from "../assets/patrocinadores/PITMA.svg";
-import selcansa from "../assets/patrocinadores/selcansa.jpg";
+import disgarsa from "../assets/patrocinadores/disgarsa.jpg";
+import emi from "../assets/patrocinadores/emi.jpg";
+import escuela from "../assets/patrocinadores/escuela.jpg";
+import pitma from "../assets/patrocinadores/pitma.jpg";
+import selcansa from "../assets/patrocinadores/selansa.jpg";
+import bm from "../assets/patrocinadores/bm.jpg";
+import ceinor from "../assets/patrocinadores/ceinor.jpg";
+import dromedario from "../assets/patrocinadores/dromedario.jpg";
+import lis from "../assets/patrocinadores/lis.jpg";
+import ubu from "../assets/patrocinadores/ubud.jpg";
 
 const patrocinadoresData = [
     { img: disgarsa },
     { img: emi },
     { img: escuela },
     { img: pitma },
-    { img: selcansa }
+    { img: selcansa },
+    { img: bm },
+    { img: ceinor },
+    { img: dromedario },
+    { img: lis },
+    { img: ubu }
 ];
 
 export default patrocinadoresData;
